ba54d1a2fc95b047e196a995cd71f81d23f71c47
TayamaYuto <tayama.yuto@icloud.com>
2023-07-20 21:54:23 +0900

    fix

---
+++ llm-server/src/llm_server/main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel, Field
+
+from llm_server.simple_agent import ask_question
+
+app = FastAPI()
+
+origins = ["http://localhost:5173"]# Frontendのオリジンをここに書く
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+class Message(BaseModel):
+    text: str = Field(title="Request message to LLM.", max_length=1000)
+
+
+class LLMResponse(BaseModel):
+    text: str
+
+
+@app.get("/healthcheck")
+def healthcheck():
+    return {}
+
+
+@app.post("/llm")
+async def run_llm(message: Message) -> LLMResponse:
+    answer = ask_question(message.text)
+    return LLMResponse(text=answer)
